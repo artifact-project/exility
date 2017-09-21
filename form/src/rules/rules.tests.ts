0bb348ed4ea188f444a3ff78256b7e84ad55c509
RubaXa <k.lebedev@corp.mail.ru>
2017-09-21 10:45:41 +0300

    * draft

--- form/src/rules/rules.tests.ts
+++ form/src/rules/rules.tests.ts
@@ -1,5 +1,33 @@
-import sum from './rules';
+import {required, minLength, email, password, ValueBox} from './rules';
 
-it('adds 1 + 2 to equal 3', () => {
-	expect(sum(1, 2)).toBe(3);
+it('minLength', () => {
+	expect(minLength(2)({value: 'a'})).toBe('minLength');
+	expect(minLength(2)({value: 'ab'})).toBe(null);
+});
+
+it('required', () => {
+	expect(required()({value: ''})).toBe('required.minLength');
+	expect(required()({value: 'a'})).toBe(null);
+});
+
+it('email', () => {
+	expect(email()({value: ''})).toBe('email.regexp');
+	expect(email()({value: 'a'})).toBe('email.regexp');
+	expect(email()({value: 'a@a.r'})).toBe(null);
+	expect(email()({value: '@a.r'})).toBe('email.regexp');
+	expect(email()({value: 'a@@a.r'})).toBe('email.regexp');
+});
+
+it('password', () => {
+	expect(password()({value: ''})).toBe('password.required.minLength');
+	expect(password()({value: '123'})).toBe('password.minLength');
+	expect(password()({value: '123456'})).toBe(null);
+});
+
+it('password: additionalRules', () => {
+	const additionalRules = ({value}: ValueBox) => /^\d+$/.test(value) ? 'onlyNumbers' : null;
+
+	expect(password(additionalRules)({value: '123456'})).toBe('password.onlyNumbers');
+	expect(password(additionalRules)({value: 'a1234'})).toBe('password.minLength');
+	expect(password(additionalRules)({value: 'a1234dddd'})).toBe(null);
 });
--- form/src/rules/rules.ts
+++ form/src/rules/rules.ts
@@ -1,57 +1,45 @@
 export type ValueBox = {
-	value: string;
-	checked: boolean;
-	selectedIndex: number;
+	value?: string;
+	checked?: boolean;
+	selectedIndex?: number;
 }
 
-export type ValidateResult = null | {
-	id: string;
-	message?: string;
-};
+export type Validity = string | null;
+export type Validation = (vbox: ValueBox) => Validity;
 
-export type Validator = (vbox: ValueBox) => boolean;
-export type ValidateRule = (vbox: ValueBox) => ValidateResult;
+function compose(id, rules: Validation[]): Validation {
+	return (vbox) => {
+		for (let i = 0; i < rules.length; i++) {
+			const invalid = rules[i](vbox);
 
-export function createRule<T>(id, configurate): (value?: T) => ValidateRule {
-	return function (value: T) {
-		const validate = configurate(value);
-		return (vbox) => validate(vbox) ? {id} : null;
+			if (invalid) {
+				return `${id}.${invalid}`;
+			}
+		}
+
+		return null;
 	};
 }
 
-function convert(validate, id) {
-	return (vbox) => ({...validate(vbox), id});
+export function regexp(pattern: RegExp): Validation {
+	return ({value}) => pattern.test(value) ? null : 'regexp';
 }
 
-function some(...rules) {
-	return (vbox) => rules.map(validate => {
-		const x = validate(vbox);
-		return x.state ? false : x;
-	});
+export function minLength(min: number): Validation {
+	return ({value}) => value.length >= min ? null : 'minLength';
 }
 
-export function regexp(pattern: RegExp) {
-	return ({value}) => ({
-		id: 'regexp',
-		state: pattern.test(value),
-	});
+export function required(): Validation {
+	return compose('required', [minLength(1)]);
 }
 
-export function minLength(min: number) {
-	return ({value}) => ({
-		id: 'minLength',
-		state: value.length >= min,
-	});
+export function email(): Validation {
+	return compose('email', [regexp(/^.@[^@]+\..+$/)]);
 }
 
-export function required() {
-	return convert(minLength(1), 'required');
-}
-
-export function password() {
-	return some(required(), minLength(6));
-}
-
-export function email() {
-	return convert(regexp(/.@.+\..+/), 'email');
+export function password(additionalRules: Validation[] = []): Validation {
+	return compose('password', [
+		required(),
+		minLength(6),
+	].concat(additionalRules));
 }
